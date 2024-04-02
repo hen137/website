f89c77ed4bbcf21c6ef39bc3aef1930268fdf4e5
Henry Abramovich <henry.abramovich@gmail.com>
2024-04-01 23:48:03 -0400

    added dimentions to video tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <video disablePictureInPicture loop muted>
+      <video disablePictureInPicture loop muted height='320' width='480'>
         <source src='/video' type='video/mp4'/>
         Video Tag is unsupported for this browser
       </video>
