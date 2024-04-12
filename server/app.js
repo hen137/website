069c41dcb80c8576e967edb6397bfd666721b0c4
Henry Abramovich <henry.abramovich@gmail.com>
2024-04-12 02:17:23 -0400

    debug changes

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require('express');
 const fs = require('fs');
+const aws = require('aws-sdk');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const s3 = new aws.S3();
 
 app.use(express.static("build"));
 
@@ -27,7 +29,7 @@ app.get("/", (req, res) => {
 app.get('/video', (req, res) => {
     const videoFile = '/media/The Most Misunderstood Concept in Physics.mp4';
     let  videoPath = './build' + videoFile;
-    console.log("A " + process.env.NODE_ENV);
+    console.log("A " + videoFile);
     const videoSize = fs.statSync(videoPath).size;
 
     console.log("B " + videoSize);
@@ -60,6 +62,49 @@ app.get('/video', (req, res) => {
         fs.createReadStream(videoPath).pipe(res);
     }
 });
+
+// app.get('/videoS3', (req, res) => {
+
+//     const bucket = 'dmosh-media';
+//     const key = '';
+
+
+//     const videoFile = '/media/The Most Misunderstood Concept in Physics.mp4';
+//     let  videoPath = './build' + videoFile;
+//     console.log("A " + videoFile);
+//     const videoSize = fs.statSync(videoPath).size;
+
+//     console.log("B " + videoSize);
+//     const MAX_CHUNK = 10 ** 6; // 1 MB
+//     const range = req.headers.range;
+//     if (range) {
+//         console.log("C");
+//         const parts = range.replace(/bytes=/, '').split('-');
+//         const start = parseInt(parts[0], 10);
+//         const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
+//         const chunksize = end - start + 1;
+
+//         const head = {
+//             'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+//             'Accept-Ranges': 'bytes',
+//             'Content-Length': chunksize,
+//             'Content-Type': 'video/mp4',
+//         };
+
+//         res.writeHead(206, head);
+//         fs.createReadStream(videoPath, { start, end }).pipe(res);
+//     } else {
+//         console.log("D");
+//         const head = {
+//             'Content-Length': videoSize,
+//             'Content-Type': 'video/mp4',
+//         };
+
+//         res.writeHead(200, head);
+//         fs.createReadStream(videoPath).pipe(res);
+//     }
+// });
+
 
 app.listen(PORT, () => console.log("Server started"));
 
