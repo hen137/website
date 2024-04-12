3cfdca9555ba5fd1a59d6ed2210d84f2837f1892
Henry Abramovich <henry.abramovich@gmail.com>
2024-04-12 03:27:09 -0400

    buckets

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 
 app.get('/video', (req, res) => {
     const videoFile = '/media/The Most Misunderstood Concept in Physics.mp4';
-    let  videoPath = './build' + videoFile;
+    let videoPath = './build' + videoFile;
     console.log("A " + videoFile);
     const videoSize = fs.statSync(videoPath).size;
 
@@ -63,47 +63,21 @@ app.get('/video', (req, res) => {
     }
 });
 
-// app.get('/videoS3', (req, res) => {
+app.get('/videoS3', (req, res) => {
 
-//     const bucket = 'dmosh-media';
-//     const key = '';
+    const bucket = 'dmosh-media';
+    const key = 'Mindful_Consumer_Podcast_Ep1video.mp4';
 
+    var params = {
+        Bucket: bucket,
+        Key: key
+    };
 
-//     const videoFile = '/media/The Most Misunderstood Concept in Physics.mp4';
-//     let  videoPath = './build' + videoFile;
-//     console.log("A " + videoFile);
-//     const videoSize = fs.statSync(videoPath).size;
-
-//     console.log("B " + videoSize);
-//     const MAX_CHUNK = 10 ** 6; // 1 MB
-//     const range = req.headers.range;
-//     if (range) {
-//         console.log("C");
-//         const parts = range.replace(/bytes=/, '').split('-');
-//         const start = parseInt(parts[0], 10);
-//         const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
-//         const chunksize = end - start + 1;
-
-//         const head = {
-//             'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-//             'Accept-Ranges': 'bytes',
-//             'Content-Length': chunksize,
-//             'Content-Type': 'video/mp4',
-//         };
-
-//         res.writeHead(206, head);
-//         fs.createReadStream(videoPath, { start, end }).pipe(res);
-//     } else {
-//         console.log("D");
-//         const head = {
-//             'Content-Length': videoSize,
-//             'Content-Type': 'video/mp4',
-//         };
-
-//         res.writeHead(200, head);
-//         fs.createReadStream(videoPath).pipe(res);
-//     }
-// });
+    s3.getObject(params, (err, data) => {
+        if (err) console.log(err, err.stack);
+        else console.log(data);
+    }).createReadStream(res);
+}); 
 
 
 app.listen(PORT, () => console.log("Server started"));
