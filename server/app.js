1b7026b31a2ed49a0ffedbf51e77a75ddaa2c94f
henry <henry.abramovich@gmail.com>
2024-04-17 17:17:01 -0400

    consolidated express routes

--- server/app.js
+++ server/app.js
@@ -1,23 +1,8 @@
-// require("dotenv").config();
-
 import express from 'express';
-import fs from 'fs';
 import got from 'got';
-import { S3Client, GetObjectAclCommand } from '@aws-sdk/client-s3';
-import dotenv from 'dotenv';
-
-// const express = require('express');
-// const fs = require('fs');
-// const got = require('got');
-// const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-
-dotenv.config();
-
-const app = express();
 
 const PORT = process.env.PORT || 8080;
-
-const s3c = new S3Client({ region: 'us-east-2' });
+const app = express();
 
 app.use(express.static("build"));
 
@@ -34,68 +19,20 @@ app.use(express.static("build"));
 // Routes
 app.get("/", (req, res) => {
     res.sendFile("index.html");
-})
-
-app.get('/video', (req, res) => {
-    const videoFile = '/media/The Most Misunderstood Concept in Physics.mp4';
-    let videoPath = './build' + videoFile;
-    console.log("A " + videoFile);
-    const videoSize = fs.statSync(videoPath).size;
-
-    console.log("B " + videoSize);
-    const MAX_CHUNK = 10 ** 6; // 1 MB
-    const range = req.headers.range;
-    if (range) {
-        console.log("C");
-        const parts = range.replace(/bytes=/, '').split('-');
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
-        const chunksize = end - start + 1;
-
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-
-        res.writeHead(206, head);
-        fs.createReadStream(videoPath, { start, end }).pipe(res);
-    } else {
-        console.log("D");
-        const head = {
-            'Content-Length': videoSize,
-            'Content-Type': 'video/mp4',
-        };
-
-        res.writeHead(200, head);
-        fs.createReadStream(videoPath).pipe(res);
-    }
-});
-
-app.get('/videoS3', async (req, res) => {
-    const command = new GetObjectCommand({
-        Bucket: 'dmosh-media',
-        Key: 'Mindful_Consumer_Podcast_Ep1video.mp4',
-        Credentials: {
-
-        }
-    });
-    const item = await s3c.send(command);
-    item.Body.pipe(res);
-
 });
 
 app.get('/videoURL', (req, res) => {
     const downloadStream = got.stream("https://dmosh-media.s3.us-east-2.amazonaws.com/Mindful_Consumer_Podcast_Ep1video.mp4");
 
-    downloadStream.on("downloadProgress", ({ transferred, total, percent }) => {
-        const percentage = Math.round(percent * 100);
-        console.error(`progress: ${transferred}/${total} (${percentage}%)`);
-    }).on("error", (error) => {
-        console.error(`Download failed: ${error.message}`);
-    });
-
+    console.log("NODE_ENV: " + process.env.NODE_ENV);
+    if (process.env.NODE_ENV === "dev") {
+        downloadStream.on("downloadProgress", ({ transferred, total, percent }) => {
+            const percentage = Math.round(percent * 100);
+            console.error(`progress: ${transferred}/${total} (${percentage}%)`);
+        }).on("error", (error) => {
+            console.error(`Download failed: ${error.message}`);
+        });
+    }
     downloadStream.pipe(res);
 });
 
